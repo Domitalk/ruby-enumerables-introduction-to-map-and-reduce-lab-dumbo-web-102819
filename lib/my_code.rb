1209443636fe85efe13e0875dc2d286b3dd95daf
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 06:46:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,7 +29,7 @@ def map_to_square(source_array)
   array2
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = "0")
   counter = starting_point
   total_temp = 0
   while counter < source_array.length do
