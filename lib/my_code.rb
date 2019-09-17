2edf98de873e005a56f4eb8be7630fa7206c3bdb
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 06:37:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,11 @@ def map_to_no_change(source_array)
   }
   return source_array
 end
+
+def map_to_double(source_array)
+  array1 = []
+  source_array.length.times { |index|
+    array1 = (source_array[index] * 2)
+  }
+  return array1
+end
