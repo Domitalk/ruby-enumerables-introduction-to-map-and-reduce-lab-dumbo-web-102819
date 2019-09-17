2841f01526d9c0dea393110505d4f6e355cc51db
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 06:36:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# My Code here....
+def map_to_negativize(source_array)
+  source_array_1 = []
+  source_array.length.times { |index|
+     source_array_1[index] = (source_array[index] * -1)
+  }
+  return source_array_1
+end
+
+def map_to_no_change(source_array)
+  source_array.length.times { |index|
+    source_array[index] = source_array[index]
+  }
+  return source_array
+end
