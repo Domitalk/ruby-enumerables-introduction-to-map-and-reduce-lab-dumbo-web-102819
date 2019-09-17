d4831a520b4e6d35719ba5583ab04a8d2878c6be
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 07:14:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-    while (source_array[i] != nil || source_array[i] != false) do
+    while !(source_array[i] == nil || source_array[i] == false) do
       i += 1
     end
   end
