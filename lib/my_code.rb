8c8bc8f3b904804d6ab968b9fc99d4a2825c72c5
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 06:41:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,22 +3,22 @@ def map_to_negativize(source_array)
   source_array.length.times { |index|
      source_array_1[index] = (source_array[index] * -1)
   }
-  return source_array_1
+  source_array_1
 end
 
 def map_to_no_change(source_array)
   source_array.length.times { |index|
     source_array[index] = source_array[index]
   }
-  return source_array
+  source_array
 end
 
 def map_to_double(source_array)
   array1 = []
   source_array.length.times { |index|
-    array1[index] = (source_array[index] * 2)
+    array1[index] = source_array[index] * 2
   }
-  return array1
+  array1
 end
 
 def map_to_square(source_array)
